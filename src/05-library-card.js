aed46133a17eb19cc2a8f3ae6048ef489cc18f44
Dayanand Singh <userjustname@gmail.com>
2026-02-09 23:55:43 +0530

    Complete conditionals lab:- 05-library-card.js  DONE

--- src/05-library-card.js
+++ src/05-library-card.js
@@ -1,4 +1,4 @@
-/**
+/** npm test -- 05-library-card
  * 📚 Maple Town Library
  *
  * The librarian at Maple Town Public Library is tired of manually checking
@@ -34,4 +34,13 @@
  */
 export function canBorrowBook(memberAge, hasValidCard, overdueBooks) {
   // Your code here
+  if (memberAge < 6) {
+    return { allowed: false, message: "Too young - must be at least 6 years old"}
+  } else if (!hasValidCard) {
+    return { allowed: false, message: "Invalid library card - please renew at the front desk" }
+  } else if (overdueBooks > 0) {
+    return { allowed: false, message: `Please return your ${overdueBooks} overdue book(s) first` }
+  } else {
+    return { allowed: true, message: "You may borrow up to 3 books" }
+  }
 }
